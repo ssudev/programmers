864e2587740a1aaeb55583d57968a20e479f7c7d
ssudev <goodys1011@naver.com>
2021-01-12 22:22:51 +0900

    주식가격

---
+++ level2/주식가격/solution.py
@@ -0,0 +1,16 @@
+def solution(prices):
+    answer = [len(prices) -i -1 for i in range(len(prices))]
+    stack = [0]
+    
+    for i in range(1,len(prices)):
+        while stack:
+            index = stack[-1]
+            
+            if prices[index] > prices[i]:
+                answer[index] = i - index
+                stack.pop()
+            else:
+                break
+        stack.append(i)
+    
+    return answer
